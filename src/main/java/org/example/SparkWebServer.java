f6eda1a4568ac01de3a19b6f60a2c9ce7d7d4858
unknown <julian.castillo-s@labinfo.is.escuelaing.edu.co>
2023-09-26 15:00:48 -0500

    initialize roundBound

---
+++ src/main/java/org/example/SparkWebServer.java
@@ -0,0 +1,28 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+
+
+import static spark.Spark.*;
+
+
+
+public class SparkWebServer {
+
+    public static void main(String... args){
+        port(getPort());
+        staticFiles.location("/public");
+        get("/roundboundalg", (req,res) -> "not supported yet");
+    }
+
+    private static int getPort() {
+        if (System.getenv("PORT") != null) {
+            return Integer.parseInt(System.getenv("PORT"));
+        }
+        return 4567;
+    }
+
+}
